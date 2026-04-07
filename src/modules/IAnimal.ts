a94fa8d17c8c32d4b7bad5e83dce593f65123a3c
hannakinnander <kinnander95@live.se>
2026-04-07 10:46:22 +0200

    added interface for animals in IAnimal.ts

--- src/modules/IAnimal.ts
+++ src/modules/IAnimal.ts
@@ -1,1 +1,10 @@
-export interface Animal {}
+export interface IAnimal {
+    name: string,
+    kindOfAnimal: string,
+    job: string,
+    skills?: string | string[],
+    birthYear: string,
+    imgUrl: string,
+    employmentStartDate: string,
+    employmentEndDate?: string
+}
