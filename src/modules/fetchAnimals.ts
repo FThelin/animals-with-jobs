68d118164c7babc87a94d14590e103df612192a2
oskar <oskarl1@live.com>
2026-04-08 14:06:18 +0200

    forgot to branch out. fetch function done in module.

--- src/modules/fetchAnimals.ts
+++ src/modules/fetchAnimals.ts
@@ -2,4 +2,21 @@
 //Använd try/catch och om anropet lyckas, returnera datan. Annars, returnera "null"
 //Typa upp funktionen med vad den returnerar
 
-export default async function fetchAnimals() {}
+import { Animal } from "./IAnimal";
+
+export default async function fetchAnimals(): Promise<Animal[] | null> {
+    try {
+        const response = await fetch("../data/data.json")
+
+        if (!response.ok) {
+            throw new Error(`Error when fetching! Status: ${response.status}`)
+        }
+
+        const data: Animal[] = await response.json()
+
+        return data
+    } catch (error) {
+        console.log("failed to fetch animals:", error)
+        return null
+    }
+}
